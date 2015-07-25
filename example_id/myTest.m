0ba392b382d6656afaca5b55b908a89c5b153cc1
APMonitor <john@apmonitor.com>
2015-07-25 16:08:56 -0600

    Linear time invariant identification
    Creates discrete and continuous state space models. Run apm_id with data
    file input.

---
+++ example_id/myTest.m
@@ -0,0 +1,20 @@
+clear all; close all; clc
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Configuration
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+obj_scale = 1;
+% number of terms
+ny = 3; % output coefficients
+nu = 3; % input coefficients
+% number of inputs
+ni = 2;
+% number of outputs
+no = 2;
+% load data and parse into columns
+load test_data.csv
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% generate state space model
+addpath('apm')
+sysd = apm_id(test_data,ni,nu,ny);
